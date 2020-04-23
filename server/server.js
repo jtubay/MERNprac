01040aa94dda7d4311088ca5b08823c03ca4c033
Jean Tubay <jean.tubay.92@gmail.com>
2020-04-22 23:20:08 -0400

    testing server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const app = express();
+const log = console.log;
 
 const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => log(`SERVER STARTED at https://http://localhost:${PORT} <====`))
