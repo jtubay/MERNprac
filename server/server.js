d778d2f650bc640a1f5834951ac753521930d132
Jean Tubay <jean.tubay.92@gmail.com>
2020-04-22 23:22:27 -0400

    testing simple route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 const log = console.log;
 
 const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 app.listen(PORT, () => log(`SERVER STARTED at https://http://localhost:${PORT} <====`))
